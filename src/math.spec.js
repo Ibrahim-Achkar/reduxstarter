05d1cee7550e22955be47dbf1b7eee59c801cfd4
Development-Person <ibaemail@gmail.com>
2020-11-24 16:46:31 +1100

    Testing - Lesson 4 - Your First Unit Test

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,13 @@
-it('first test', () => {});
+import { isEven } from './math';
+
+describe('isEven', () => {
+  it('should return true if given an even number', () => {
+    const result = isEven(2);
+    expect(result).toEqual(true);
+  });
+
+  it('should return false if given an odd number', () => {
+    const result = isEven(1);
+    expect(result).toEqual(false);
+  });
+});
