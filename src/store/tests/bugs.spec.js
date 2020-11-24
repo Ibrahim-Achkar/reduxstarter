6a0dac6c593e10ee652505e1c63a079f6e4d631d
Development-Person <ibaemail@gmail.com>
2020-11-24 21:25:52 +1100

    Testing - Lesson 8 - Mocking HTTP Calls

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,11 +1,16 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addBug } from '../bugs';
 import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
   it('should handle the addBug action', async () => {
-    const store = configureStore();
     const bug = { description: 'a' };
+    const savedBug = { ...bug, id: 1 };
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost('/bugs').reply(200, savedBug);
+    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
