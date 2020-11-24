7ba9df37a50509dc134476112c3b7ba67a1c4b43
Development-Person <ibaemail@gmail.com>
2020-11-24 21:15:50 +1100

    Testing - Lesson 7 - Social Tests

--- src/store/middleware/toast.js
+++ src/store/middleware/toast.js
@@ -1,6 +1,6 @@
 const toast = (state) => (next) => (action) => {
   if (action.type === 'error') console.log(`Toastify`, action.payload.message);
-  else next(action);
+  else return next(action);
 };
 
 export default toast;
--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,21 +1,11 @@
-import { addBug, bugAdded } from '../bugs';
-import { apiCallBegan } from '../apiactions';
+import { addBug } from '../bugs';
+import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
-  describe('action creators', () => {
-    it('addBug', () => {
-      const bug = { description: 'a' };
-      const result = addBug(bug);
-      const expected = {
-        type: apiCallBegan.type,
-        payload: {
-          url: '/bugs',
-          method: 'post',
-          data: bug,
-          onSuccess: bugAdded.type,
-        },
-      };
-      expect(result).toEqual(expected);
-    });
+  it('should handle the addBug action', async () => {
+    const store = configureStore();
+    const bug = { description: 'a' };
+    await store.dispatch(addBug(bug));
+    expect(store.getState().entities.bugs.list).toHaveLength(1);
   });
 });
