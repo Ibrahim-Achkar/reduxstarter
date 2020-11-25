67538fc63b308be317a767c59308bc8b0162663d
Development-Person <ibaemail@gmail.com>
2020-11-25 11:01:53 +1100

    Testing - Lesson 9 - Writing Clean Tests

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -4,13 +4,38 @@ import { addBug } from '../bugs';
 import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
-  it('should handle the addBug action', async () => {
+  let fakeAxios;
+  let store;
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureStore();
+  });
+
+  const bugsSlice = () => store.getState().entities.bugs;
+
+  it(`should add the bug to the store if it's saved to the server`, async () => {
+    //Arrange
     const bug = { description: 'a' };
     const savedBug = { ...bug, id: 1 };
-    const fakeAxios = new MockAdapter(axios);
     fakeAxios.onPost('/bugs').reply(200, savedBug);
-    const store = configureStore();
+
+    //Act
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    //Assert
+    expect(bugsSlice().list).toContainEqual(savedBug);
+  });
+
+  it(`should not add the bug to the store if it's not saved to the server`, async () => {
+    //Arrange
+    const bug = { description: 'a' };
+    fakeAxios.onPost('/bugs').reply(500);
+
+    //Act
+    await store.dispatch(addBug(bug));
+
+    //Assert
+    expect(bugsSlice().list).toHaveLength(0);
   });
 });
